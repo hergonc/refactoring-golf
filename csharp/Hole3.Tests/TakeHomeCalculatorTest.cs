d9829b36990f6b2a4dd3d0444c7ab779ae9c6486
Oscar Herrero <oherrero@aidacanarias.com>
2021-11-03 16:46:40 +0100

    Repair test for hole3

--- csharp/Hole3.Tests/TakeHomeCalculatorTest.cs
+++ csharp/Hole3.Tests/TakeHomeCalculatorTest.cs
@@ -8,16 +8,17 @@ namespace Hole3.Tests
         [Fact]
         public void CanCalculateTax()
         {
-            int first = new TakeHomeCalculator(10).NetAmount(Money.CreateInstance(40, "GBP"),
-                Money.CreateInstance(50, "GBP"),
-                Money.CreateInstance(60, "GBP")).value;
+            int first = new TakeHomeCalculator(10).NetAmount(new Money(40, "GBP"),
+                new Money(50, "GBP"),
+                new Money(60, "GBP")).value;
             Assert.Equal(135, first);
         }
 
         [Fact]
         public void CannotSumDifferentCurrencies()
         {
-            Assert.Throws<Incalculable>(() => new TakeHomeCalculator(10).NetAmount(Money.CreateInstance(4, "GBP"), Money.CreateInstance(5, "USD")));
+            Assert.Throws<Incalculable>(() => new TakeHomeCalculator(10).NetAmount(new
+                Money(4, "GBP"), new Money(5, "USD")));
         }
     }
 }
